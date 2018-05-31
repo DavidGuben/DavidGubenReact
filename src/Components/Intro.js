5b22980c68145284836cc2567abd475f51b71347
DavidGuben <Davidjg6@gmail.com>
2018-05-30 23:46:09 -0400

    added logo and new intro img

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react'
 import {View, Mask} from 'mdbreact'
-import './../App.css';
+
+import './../App.css'
+import Logo from './../Images/logo.svg'
 
 class Intro extends Component {
     render() {
         return (
             <View className="hm-blue-strong">
                 <div className="intro z-depth-1 animated fadeIn">
-                <Mask className="" />
+                <img src={Logo} alt="logo" width="100" height="100" className="App-logo" />
+                <Mask />
                 <div className="row">
                 <div className="col-md-12" style={{marginTop: '350px'}}>
                     
