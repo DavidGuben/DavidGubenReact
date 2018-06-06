c27aa6be72c0ae3232685164288b6e46d7234471
DavidGuben <Davidjg6@gmail.com>
2018-06-05 22:29:33 -0400

    seperated services into seperate components and setup children element location for different icons

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,45 +1,40 @@
-import React, {Component} from 'react';
-import {Container} from 'mdbreact';
+import React, {Component} from 'react'
+import {Container} from 'mdbreact'
+
+import Service from './Service'
 
 class Features extends Component {
     render() {
         return (
             <Container>
-
             <div id="best-features" className="text-center">
-
                 <h2 style={{ marginTop: '20px' }}>Services</h2>
-
                 <div className="row d-flex justify-content-center mb-4">
-
                 <div className="col-md-8">
-
                     <p className="grey-text">Website creation</p>
-
+                </div>
                 </div>
 
-            </div>
+                <div className="row">
+                <Service
+                    heading="Design"
+                    description="Design idea">
+                    <i className="fa fa-pencil fa-4x blue-text"></i>
+                </Service>
 
-            <div className="row">
+                <Service
+                    heading="Develop"
+                    description="Develop idea">
+                    <i className="fa fa-code fa-4x blue-text"></i>
+                </Service>    
 
-            <div className="col-md-4 mb-5">
-                <i className="fa fa-pencil fa-4x lime-text"></i>
-                <h4 className="my-4 font-weight-bold">Design</h4>
-                <p className="grey-text">Imagine idea</p>
-            </div>
 
-            <div className="col-md-4 mb-1">
-                <i className="fa fa-code fa-4x lime-text"></i>
-                <h4 className="my-4 font-weight-bold">Develop</h4>
-                <p className="grey-text">Create idea</p>
-            </div>
-
-            <div className="col-md-4 mb-1">
-                <i className="fa fa-thumbs-up fa-4x lime-text"></i>
-                <h4 className="my-4 font-weight-bold">Publish</h4>
-                <p className="grey-text">Deploy idea</p>
-            </div>
-            </div>
+                <Service
+                    heading="Publish"
+                    description="Publish idea">
+                    <i className="fa fa-thumbs-up fa-4x blue-text"></i>
+                </Service>
+                </div>
             </div>
             </Container>
         )
