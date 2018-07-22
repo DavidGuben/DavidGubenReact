1e3bb9d153b252ea354aebafc972b2c3b2c4dc68
David Guben <davidguben@Davids-MBP.fios-router.home>
2018-07-21 20:47:55 -0400

    removed scss from config and app

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -2,11 +2,6 @@ import React, {Component} from 'react'
 import {View} from 'mdbreact'
 
 import './../App.css'
-import './../Loaders.scss'
-import Loader from 'react-loaders'
-
-let loader = <Loader type="line-scale" className="loader loader-active animated fadeIn" />
-
 
 class Intro extends Component {
     render() {
@@ -15,7 +10,7 @@ class Intro extends Component {
                 <div className="intro z-depth-1 animated fadeIn">
                     <div className="row">
                     <div className="col-md-12 logo-container">
-                        {loader}
+                        
                     </div>
                     </div>
                 </div>
