519be5c66c89b2dc1200600de07e2faff9229053
David Guben <davidjg6@gmail.com>
2018-07-27 00:06:01 -0400

    building card: added 3 boxes to display technologies used

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -11,11 +11,20 @@ class Intro extends Component {
             <View className="hm-blue-strong">
                 <div className="intro z-depth-1-half animated fadeIn">
                     <div className="row">
+
                     <div className="col-md-12 d-flex justify-content-center">
                         <div className="intro-content text-center">       
                             <IntroCard />
                         </div>
                     </div>
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <div className="d-flex intro-description">
+                            <div>one</div>
+                            <div>two</div>
+                            <div>three</div>
+                        </div>
+                    </div>
+
                     </div>
                 </div>
             </View>
