ae15ff74cd266333dc10236ec16787ed81a5fd3b
David Guben <davidjg6@gmail.com>
2018-07-30 23:51:22 -0400

    creating better herocard

--- src/Components/Intro/HeroCard.js
+++ src/Components/Intro/HeroCard.js
@@ -7,16 +7,19 @@ class HeroCard extends Component {
     render() {
         return (
             <div className="d-flex">
-                <div className="row">
-                <div className="text-center"> 
-                    <div className="col-md-12 intro-header">
-                        <h1><b>David Guben</b></h1>
-                        <p>React Web Developer</p>
-                        <img src={ReactLogo} alt="react" className="animated rotate infinite"/>
+                <div className="row text-center">
+                
+                    <div className="intro-header-container animated bounceIn">
+                        <div className="col-md-12 intro-header">
+                            <h1><b>David Guben</b></h1>
+                            <p>App & Web Developer</p>
+                            <img src={ReactLogo} alt="react" className="animated rotate infinite"/>
+                        </div>
                     </div>
-                    
-                    <div className="col-md-12 intro-content">React Developer</div>
-                </div>
+
+                    <div className="col-md-12 intro-content animated bounceIn">
+                        <p>I create and design apps & websites.</p>
+                    </div>
                 </div>
             </div>
         );
