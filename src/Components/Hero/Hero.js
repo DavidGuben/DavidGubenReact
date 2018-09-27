1966025c836c2c9d568ee93d12c88a7440715f45
David Guben <davidjg6@gmail.com>
2018-09-27 19:22:35 -0400

    removed video from hero because of lag

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,16 +2,12 @@ import React, {Component} from 'react';
 import {View} from 'mdbreact';
 
 import HeroCard from '../HeroCard/HeroCard';
-import animatedSmoke from './../../Images/AnimatedSmoke_LQ.mp4';
 import './Hero.css';
 
 class Intro extends Component {
     render() {
         return (
             <View>
-                <video width="3000vh" style={{position: 'absolute', opacity: 0.4}} autoPlay loop>
-                    <source src={animatedSmoke} type="video/mp4"></source>
-                </video>
                 <div className="intro z-depth-1-half animated fadeIn">
                     <HeroCard />
                 </div>
